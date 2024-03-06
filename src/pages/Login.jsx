7be57601e9065f895c5947e313cccb2b28807727
Sharon Cartagena <sharoncartagenabarahona@gmail.com>
2024-03-06 15:21:21 -0600

    Login
    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,133 @@
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from '@mui/material/Alert';
+
+const temaLogin = createTheme();
 
 export const Login = () => {
+  const [usuario, setUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [mensajeValidacion, setMensajeValidacion] = useState('');
+
+  const user = 'usuario';
+  const contrasenia = '1234';
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (usuario === user && contrasena === contrasenia) {
+      setMensajeValidacion('Usuario validado');
+    } else {
+      setMensajeValidacion('Usuario o contraseña incorrectos');
+    }
+  };
+
   return (
-    <> 
-    <h1>Login</h1>
- </>
-  )
+    <ThemeProvider theme={temaLogin}>
+      <Box
+        sx={{
+          backgroundColor: '#819992',
+          minHeight: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              backgroundColor: 'white',
+              padding: '20px',
+              borderRadius: '8px',
+              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Ingreso
+              </Typography>
+              {mensajeValidacion && <Alert severity={mensajeValidacion === 'Usuario validado' ? 'success' : 'error'}>{mensajeValidacion}</Alert>}
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="user"
+                  label="Usuario"
+                  name="usuario"
+                  autoComplete="username"
+                  autoFocus
+                  onChange={(e) => setUsuario(e.target.value)}
+                  sx={{
+                    '& .MuiOutlinedInput-root': {
+                      borderColor: '#70AD5F', // Color del borde normal
+                    },
+                    '& .MuiOutlinedInput-root:hover': {
+                      borderColor: '#70AD5F', // Color del borde al pasar el ratón
+                    },
+                    '& .Mui-focused .MuiOutlinedInput-root': {
+                      borderColor: '#70AD5F', // Color del borde enfocado
+                    },
+                  }}
+                />
+
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Contraseña"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(e) => setContrasena(e.target.value)}
+                  sx={{
+                    '& .MuiOutlinedInput-root': {
+                      borderColor: '#70AD5F', // Color del borde normal
+                    },
+                    '& .MuiOutlinedInput-root:hover': {
+                      borderColor: '#70AD5F', // Color del borde al pasar el ratón
+                    },
+                    '& .Mui-focused .MuiOutlinedInput-root': {
+                      borderColor: '#70AD5F', // Color del borde enfocado
+                    },
+                  }}
+                />
+                
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2, backgroundColor: '#70AD5F'  }}
+                >
+                  Ingresar
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </ThemeProvider>
+  );
 }
+
+
+
+

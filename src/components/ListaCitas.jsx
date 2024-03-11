494f06babc397dfc6aca002cba0e31698460ceb2
Sharon Cartagena <sharoncartagenabarahona@gmail.com>
2024-03-11 00:44:52 -0600

    Proximas citas

--- src/components/ListaCitas.jsx
+++ src/components/ListaCitas.jsx
@@ -1,6 +1,116 @@
 
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { TableVirtuoso } from 'react-virtuoso';
+
+const sample = [
+  ['21-04-2024', '12:30', 'Baxter', 'Vacunación'],
+  ['30-01-2024', '01:30', 'Firulais','Desparacitación'],
+  ['30-03-2024', '02:30', 'ayuda :(', 'Chequeo'],
+  ['30-01-2023', '05:30', 'autzilio', 'Operación'],
+  ['30-04-2023', '04:30', 'ya va dando', 'ya no se :('],
+];
+
+function createData(id, dessert, calories, fat, carbs, protein) {
+  return { id, dessert, calories, fat, carbs, protein };
+}
+
+const columns = [
+  {
+    width: 60,
+    label: 'Fecha',
+    dataKey: 'dessert',
+  },
+  {
+    width: 120,
+    label: 'Hora',
+    dataKey: 'calories',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Paciente',
+    dataKey: 'fat',
+    numeric: true,
+  },
+  {
+    width: 120,
+    label: 'Motivo',
+    dataKey: 'carbs',
+    numeric: true,
+  }
+];
+
+const rows = Array.from({ length: 50 }, (_, index) => {
+  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+  return createData(index, ...randomSelection);
+});
+
+const VirtuosoTableComponents = {
+  // eslint-disable-next-line react/display-name
+  Scroller: React.forwardRef((props, ref) => (
+    <TableContainer component={Paper} {...props} ref={ref} />
+  )),
+  Table: (props) => (
+    <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+  ),
+  TableHead,
+  // eslint-disable-next-line react/prop-types, no-unused-vars
+  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+  // eslint-disable-next-line react/display-name
+  TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
+};
+
+function fixedHeaderContent() {
+  return (
+    <TableRow>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          variant="head"
+          align={column.numeric || false ? 'right' : 'left'}
+          style={{ width: column.width }}
+          sx={{
+            backgroundColor: 'background.paper',
+          }}
+        >
+          {column.label}
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+}
+
+function rowContent(_index, row) {
+  return (
+    <React.Fragment>
+      {columns.map((column) => (
+        <TableCell
+          key={column.dataKey}
+          align={column.numeric || false ? 'right' : 'left'}
+        >
+          {row[column.dataKey]}
+        </TableCell>
+      ))}
+    </React.Fragment>
+  );
+}
+
 export const ListaCitas = () => {
   return (
-    <div>ListaCitas</div>
-  )
+    <Paper style={{ height: 400, width: '100%' }}>
+      <TableVirtuoso
+        data={rows}
+        components={VirtuosoTableComponents}
+        fixedHeaderContent={fixedHeaderContent}
+        itemContent={rowContent}
+      />
+    </Paper>
+  );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,22 @@
 
+import { ListaCitas } from "../components/ListaCitas"
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 export const Dashboard = () => {
+  const boxStyles = {
+    width: '45%', 
+    marginLeft: '2%', 
+    marginTop: '20vh',
+    textAlign: 'center' 
+  };
+
   return (
-    <> 
-       <h2>DASHBOARD</h2>
-    </>
-  )
+    <Box sx={boxStyles}>
+      <Typography variant="h4" gutterBottom>
+        Próximas Citas 
+      </Typography>
+      <ListaCitas />
+    </Box>
+  );
 }
